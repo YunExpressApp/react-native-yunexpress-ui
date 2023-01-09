2a9d19c9679f9b9f745134a614dc617f18fa4c97
康乐 <yuankangle@yunexpress.cn>
2023-01-09 13:33:13 +0800

    ui框架从kolin改为java

---
+++ android/src/main/java/com/reactnativeyunexpressui/YunexpressUiViewManager.java
@@ -0,0 +1,11 @@
+package com.reactnativeyunexpressui;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+
+public class YunexpressUiViewManager extends ReactContextBaseJavaModule {
+
+  @Override
+  public String getName() {
+    return "YunexpressUi";
+  }
+
+}
